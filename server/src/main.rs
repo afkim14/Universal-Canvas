d5d7402b981475943a270d0a03a3f499e2a6f548
Jonathan Chan <jonchanyc@outlook.com>
2018-06-04 21:33:54 -0500

    Fix warnings in main

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,3 @@
-
-use std::net::{TcpListener};
-
 // User Modules
 mod canvas;
 use canvas::*;
@@ -13,22 +10,8 @@ use ws::WebSocket;
 const LOCAL_HOST: &str = "10.105.248.182:8080";
 
 fn main() {
-    // let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    // for stream in listener.incoming() {
-    //     println!("new client\n");
-    //     let stream = stream.unwrap();
-    // }
-    let mut canvas = Canvas::new(100, 60, 10);
-    let mut server = CanvasServer::new(canvas);
-    let ws = <(WebSocket<CanvasServer>)>::new(server).unwrap();
-    // ws.bind(host).unwrap();
+    let canvas = Canvas::new(100, 60, 10);
+    let server = CanvasServer::new(canvas);
+    let ws = <WebSocket<CanvasServer>>::new(server).unwrap();
     ws.listen(LOCAL_HOST).unwrap();
 }
-
-// fn send_canvas_to_clients(canvas: &Canvas) {
-//     unimplemented!();
-// }
-
-// fn update_canvas(pixels: &Vec<Pixel>) {
-//     unimplemented!();
-// }
