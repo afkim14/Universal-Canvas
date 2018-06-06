4da24606262fb994139de5a2acd7f37127c72fa3
Jonathan Chan <jonchanyc@outlook.com>
2018-06-06 01:02:20 -0500

    Continue universe stubs

--- server-lib/src/server.rs
+++ server-lib/src/server.rs
@@ -3,23 +3,25 @@ extern crate json;
 
 use std::sync::{Arc, RwLock};
 use self::ws::{Sender, Factory, Handler, Message, Result};
-use universe::Universe;
+use std::marker::PhantomData;
+use universe::{Universe, Atom};
 
 type SharedUniverse<U> = Arc<RwLock<U>>;
 
 #[derive(Debug)]
-pub struct Server<U> {
+pub struct Server<U, A> {
     universe: SharedUniverse<U>,
+    atom_phantom: PhantomData<A>,
 }
 
-impl<U: Universe> Server<U> {
+impl<U: Universe<A>, A: Atom> Server<U, A> {
     pub fn new(universe: U) -> Self {
         Server {
             universe: Arc::new(RwLock::new(universe)),
         }
     }
 
-    pub fn as_client_handler(&self, out: Sender) -> ClientHandler<U> {
+    pub fn as_client_handler(&self, out: Sender) -> ClientHandler<U, A> {
         ClientHandler {
             out,
             universe: self.universe.clone()
@@ -27,8 +29,8 @@ impl<U: Universe> Server<U> {
     }
 }
 
-impl<U: Universe> Factory for Server<U> {
-    type Handler = ClientHandler<U>;
+impl<U: Universe<A>, A: Atom> Factory for Server<U, A> {
+    type Handler = ClientHandler<U, A>;
 
     fn connection_made(&mut self, out: Sender) -> Self::Handler {
         self.as_client_handler(out)
@@ -39,12 +41,13 @@ impl<U: Universe> Factory for Server<U> {
     }
 }
 
-pub struct ClientHandler<U> {
+pub struct ClientHandler<U, A> {
     out: Sender,
     universe: SharedUniverse<U>,
+    atom_phantom: PhantomData<A>,
 }
 
-impl<U: Universe> Handler for ClientHandler<U> {
+impl<U: Universe<A>, A: Atom> Handler for ClientHandler<U, A> where U: Universe<A>, A: Atom {
     fn on_message(&mut self, message: Message) -> Result<()> {
         unimplemented!()
     }
--- server-lib/src/universe.rs
+++ server-lib/src/universe.rs
@@ -1,11 +1,36 @@
 extern crate json;
 use self::json::JsonValue;
 
-pub trait Universe {
-    type Atom;
+use std::ops::{Index, IndexMut};
 
-    fn parse(json: &JsonValue) -> Self;
-    fn stringify(&self) -> JsonValue;
+pub trait JsonConvertible {
+    fn from_json(json: &JsonValue) -> Self;
+    fn as_json(&self) -> JsonValue;
+}
 
-    fn atom_key(&self) -> &str;
+pub trait Universe<A>: JsonConvertible {
+    fn atom_name_singular(&self) -> &str; // "pixel"
+    // fn atom_name_plural(&self) -> &str; // "pixels"
+
+    fn update_atom(&mut self, atom: A);
+
+    fn atom_with_id(&self, id: usize) -> &A;
+    fn atom_with_id_mut(&mut self, id: usize) -> &mut A;
+}
+
+impl<A> Index<usize> for Universe<A> where A: Atom {
+    type Output = A;
+    fn index(&self, id: usize) -> &Self::Output {
+        self.atom_with_id(id)
+    }
+}
+
+impl<A> IndexMut<usize> for Universe<A> where A: Atom {
+    fn index_mut(&self, id: usize) -> &mut Self::Output {
+        self.atom_with_id_mut(id)
+    }
+}
+
+pub trait Atom: JsonConvertible {
+    fn id(&self) -> &usize;
 }
